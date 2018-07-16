99f15d19ad985a89772378841695c70b88d6c875
Mustafa Raarujeed <mustafaraar@gmail.com>
2018-07-16 20:43:24 +0300

    banner and category models updated

--- app/Banner.php
+++ app/Banner.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Banner extends Model
 {
-    //
+
+	/**
+	 * [$fillable description]
+	 * @var [type]
+	 */
+	protected $fillable = [
+        'category_id',
+        'title',
+        'link',
+        'sort',
+        'desc',
+        'is_featured',
+        'is_visible',
+        'image',
+    ];
+
+
+    public function categories()
+    {
+    	return $this->belongsTo('App\Category', 'category_id');
+    }
 }
--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+
+	/**
+	 * [$fillable description]
+	 * @var [type]
+	 */
+    protected $fillable = [
+        'name_ar',
+        'name_en',
+        'is_visible',
+    ];
+
+
+    /**
+     * @return [type]
+     */
+	public function banners()
+	{
+    	return $this->hasMany('App\Banner', 'category_id', 'id');
+    }
+
 }
