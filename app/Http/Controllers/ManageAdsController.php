c05e88de2bf240a2ed7d05d773ea74055481a808
Mustafa Raarujeed <mustafaraar@gmail.com>
2018-07-17 07:58:39 +0300

    blueprint for ads management for sys admins

---
+++ app/Http/Controllers/ManageAdsController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManageAdsController extends Controller
+{
+	/**
+	 * [index description]
+	 * @return [type] [description]
+	 */
+    public function index()
+    {
+    	# code...
+    }
+
+    /* Category */
+    
+    /**
+     * [catIndex description]
+     * @return [type] [description]
+     */
+    public function catIndex()
+    {
+    	# code...
+    }
+    
+    /**
+     * [catAdd description]
+     * @return [type] [description]
+     */
+    public function catAdd()
+    {
+    	# code...
+    }
+
+    /**
+     * [catEdit description]
+     * @return [type] [description]
+     */
+    public function catEdit()
+    {
+    	# code...
+    }
+
+    /**
+     * [catUpdate description]
+     * @return [type] [description]
+     */
+    public function catUpdate()
+    {
+    	# code...
+    }
+
+    /**
+     * [catDelete description]
+     * @return [type] [description]
+     */
+    public function catDelete()
+    {
+    	# code...
+    }
+
+    /* Banner */
+
+    /**
+     * [bannerIndex description]
+     * @return [type] [description]
+     */
+    public function bannerIndex()
+    {
+    	# code...
+    }
+    
+    /**
+     * [bannerAdd description]
+     * @return [type] [description]
+     */
+    public function bannerAdd()
+    {
+    	# code...
+    }
+
+    /**
+     * [bannerEdit description]
+     * @return [type] [description]
+     */
+    public function bannerEdit()
+    {
+    	# code...
+    }
+
+    /**
+     * [bannerUpdate description]
+     * @return [type] [description]
+     */
+    public function bannerUpdate()
+    {
+    	# code...
+    }
+
+    /**
+     * [bannerDelete description]
+     * @return [type] [description]
+     */
+    public function bannerDelete()
+    {
+    	# code...
+    }
+}
