cee53a458cda6ee9dcae2bc13f07340a6b939274
Mustafa Raarujeed <mustafaraar@gmail.com>
2018-07-17 07:58:03 +0300

    blueprint for auth management of admins, managers and advertiser

---
+++ app/Http/Controllers/ManageAuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManageAuthController extends Controller
+{
+	/**
+	 * [login description]
+	 * @return [type] [description]
+	 */
+    public function login()
+    {
+    	
+    }
+
+    /**
+     * [logout description]
+     * @return [type] [description]
+     */
+    public function logout()
+    {
+    	
+    }
+}
