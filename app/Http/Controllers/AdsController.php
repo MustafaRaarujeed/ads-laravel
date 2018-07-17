30f2a14a575757cdaba7ff0c3760ede061687550
Mustafa Raarujeed <mustafaraar@gmail.com>
2018-07-17 07:56:57 +0300

    blueprint for ads controller for displaying ads to user

---
+++ app/Http/Controllers/AdsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdsController extends Controller
+{
+	/**
+	 * [index description]
+	 * @return [type] [description]
+	 */
+    public function index()
+    {
+    	
+    }
+}
