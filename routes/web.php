c6e1e984579050a2985fb3c7b725d0fe7e4a1bb7
Mustafa Raarujeed <mustafaraar@gmail.com>
2018-07-17 07:59:07 +0300

    minor changes to route files to include controllers created

--- routes/web.php
+++ routes/web.php
@@ -13,39 +13,39 @@
 
 // Front Page to display ads (categories and banners)
 Route::group(['prefix' => ''], function(){
-	Route::get('/', '');
+	Route::get('/', 'AdsController@index');
 });
 
 // Backend
 Route::group(['prefix' => '/mange'], function(){
 	// Login
-	Route::get('/', '');
+	Route::get('/', 'ManageAuthController@login');
 	
 	// Inside System Group - protected by middleware
 	Route::group(['prefix' => '/ads'], function(){
 		// View Ads(Categories and Banners)
-		Route::get('/', '');
+		Route::get('/', 'ManageAdsController@index');
 		// Add Category CRUD - protected by middleware
 		Route::group(['prefix' => '/cat'], function(){
 			// Add
-			Route::post('/add', '');
+			Route::post('/add', 'ManageAdsController@catAdd');
 			// Edit
 			// Update
 			// Delete
-			Route::delete('/del/{id}', '');
+			Route::delete('/del/{id}', 'ManageAdsController@catDelete');
 		});
 		// Add Banner CRUD - protected by middleware
 		Route::group(['prefix' => '/banner'], function(){
 			// Add
-			Route::post('/add', '');
+			Route::post('/add', 'ManageAdsController@bannerAdd');
 			// Edit
 			// Update
 			// Delete
-			Route::delete('/del/{id}', '');
+			Route::delete('/del/{id}', 'ManageAdsController@bannerDelete');
 		});
 		// Add Admins
 		// Add Advertiser
 		// Logout
-		Route::get('/logout', '');
+		Route::get('/logout', 'ManageAuthController@logout');
 	});
 });
