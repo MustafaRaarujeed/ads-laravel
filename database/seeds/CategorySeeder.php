fdcbf83e559b691a37791c75a2d08fa50ba74dbd
Mustafa Raarujeed <mustafaraar@gmail.com>
2018-07-21 19:47:06 +0300

    add a single new dummy record to category table through seeders

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -17,5 +17,11 @@ class CategorySeeder extends Seeder
         $category->name_ar = "الموضة";
         $category->is_visible = 1;
         $category->save();
+
+        $category = new Category();
+        $category->name_en = "Car";
+        $category->name_ar = "السيارات";
+        $category->is_visible = 0;
+        $category->save();
     }
 }
