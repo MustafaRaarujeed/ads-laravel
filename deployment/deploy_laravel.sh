35893e606c2a4030fa38b092b26f6f01fc4438e4
Mustafa Raarujeed <mustafaraar@gmail.com>
2018-07-21 19:35:41 +0300

    changes to deploy laravel script

--- deployment/deploy_laravel.sh
+++ deployment/deploy_laravel.sh
@@ -10,23 +10,26 @@ cd /var/www/html/
 # mkdir -p /var/www/html/storage/framework/cache
 # mkdir -p /var/www/html/public/files/
 
+# Bring down application
+php artisan down
+
 # Install dependencies
 export COMPOSER_ALLOW_SUPERUSER=1
 composer install -d /var/www/html/
 
 # Copy configuration from /var/www/.env, see README.MD for more information
-cp /var/www/html/.env.example /var/www/html/.env
+# cp /var/www/html/.env.example /var/www/html/.env
 
 # Generate key envryption
-php artisan key:generate
+# php artisan key:generate
 
 # Clear any previous cached views
-php artisan config:clear
-php artisan cache:clear
-php artisan view:clear
+# php artisan config:clear
+# php artisan cache:clear
+# php artisan view:clear
 
 # Optimize the application
-php artisan config:cache
+# php artisan config:cache
 # php /var/www/html/artisan optimize
 
 # Change rights
