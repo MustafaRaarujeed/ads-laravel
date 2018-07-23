02d5610db20a6f7fb79392fe17b3c1333fee220c
Mustafa Raarujeed <mustafaraar@gmail.com>
2018-07-23 14:52:12 +0300

    added home route to the logo

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light raar-nav">
     <div class="container">
-        <a class="navbar-brand" href="#">Raar Ads</a>
+        <a class="navbar-brand" href="{{ route('ads.index') }}">Raar Ads</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
